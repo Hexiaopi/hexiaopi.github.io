cbb54de6c7956acd2fab9118a5243959084583a9
hexiaopi <hechangjie0619@icloud.com>
2023-09-17 10:20:03 +0800

    docs: 增加文档搜索

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,6 @@
 import { defineUserConfig } from "vuepress";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
+import { cut } from "nodejs-jieba";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -9,6 +11,23 @@ export default defineUserConfig({
   description: "一枚后端攻城狮的个人博客",
 
   theme,
+
+  plugins: [
+    searchProPlugin({
+      indexContent: true,
+      hotReload: true,
+      customFields: [
+        {
+          getter: ({ frontmatter }) => frontmatter.tag as string[],
+          formatter: `Tag: $content`,
+        },
+      ],
+      indexOptions: {
+        tokenize: (text, fieldName) =>
+          fieldName === "id" ? [text] : cut(text, true),
+      },
+    }),
+  ]
 
   // Enable it with pwa
   // shouldPrefetch: false,
